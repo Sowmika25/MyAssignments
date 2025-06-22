fe368085a863145d2a1b6503b22b22c7719bff1d
Sowmika25 <84906244+Sowmika25@users.noreply.github.com>
2025-06-22 12:42:22 +0530

    Add files via upload
    week1day1

---
+++ Chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008 ;
+		char shortcutKey ='C';
+		
+		System.out.println("version : "+version+"\n"+"developer : "+developer+"\n"+"isBeta : "+isBeta+"\n"+"releaseYear : "+releaseYear+"\n"+"shortcutKey : "+shortcutKey);
+		
+	}
+
+}
